7602e5e684164958722d73949bb8b6dfde56f971
Abdul Bari <abdulbari80@gmail.com>
2025-11-12 17:27:32 +1100

    a threshold add to retriver and prompt is modifed in generator

--- src/rag/rag_pipeline.py
+++ src/rag/rag_pipeline.py
@@ -4,10 +4,17 @@ from langchain_core.documents import Document
 from src.rag.logger import get_logger
 from src.rag.exceptions import VectorStoreNotInitializedError
 from src.rag.faiss_store import FaissStore
-from src.rag.llm_connector import get_llm_response
+# from src.rag.llm_connector import get_llm_response
+from src.rag.config import settings 
+
+from langchain_ollama import ChatOllama  # or OllamaLLM
+from langchain_core.messages import SystemMessage, HumanMessage
+
+MODEL_NAME = settings.llm_model_name
+TEMPERATURE = settings.llm_temperature
+THRESHOLD = settings.threshold
 
 logger = get_logger(__name__)
-
 
 class RAGPipeline:
     """
@@ -19,8 +26,7 @@ class RAGPipeline:
         self,
         vector_store: Optional[FaissStore] = None,
         top_k: int = 3,
-        llm_model_name: Optional[str] = None,
-    ):
+        llm_model_name: Optional[str] = None):
         """
         Initialize RAGPipeline.
 
@@ -31,8 +37,7 @@ class RAGPipeline:
         """
         self.vector_store = vector_store or FaissStore()
         self.top_k = top_k
-        self.llm_model_name = llm_model_name
-        logger.info(f"RAGPipeline initialized with top_k={self.top_k}")
+        self.llm_model_name = llm_model_name or MODEL_NAME
 
     def add_documents(self, documents: List[Document]):
         """
@@ -60,7 +65,8 @@ class RAGPipeline:
             logger.info("FAISS store not initialized. Creating new store...")
             self.vector_store.create_store(documents)
 
-    def retrieve(self, query: str) -> List[Document]:
+    def filter_retrieved_context(self, query: str,
+                                 threshold: float=THRESHOLD) -> List[Document]:
         """
         Retrieve top-k most similar documents from the vector store.
 
@@ -74,12 +80,13 @@ class RAGPipeline:
             logger.error("Vector store is not initialized for retrieval.")
             raise VectorStoreNotInitializedError()
 
-        logger.info(f"Retrieving top {self.top_k} documents for query: {query}")
-        return self.vector_store.similarity_search(query, k=self.top_k)
+        sim_results = self.vector_store.similarity_search(query, k=self.top_k) 
+        filtered_context_docs = [doc for doc, score in sim_results if score > threshold]
+        return filtered_context_docs
 
-    def generate_answer(self, query: str, context_docs: List[Document]) -> str:
+    def generate_response(self, query: str, context_docs: List[Document]) -> str:
         """
-        Generate an answer using the LLM with retrieved context.
+        Generate a response using the LLM with retrieved context.
 
         Args:
             query (str): User query.
@@ -88,13 +95,29 @@ class RAGPipeline:
         Returns:
             str: LLM-generated response.
         """
-        context_text = "\n".join([doc.page_content for doc in context_docs])
-        logger.info("Generating response using LLM with retrieved context...")
-        response = get_llm_response(prompt=query, model_name=self.llm_model_name)
-        logger.info("Response generation complete.")
-        return response
+        if context_docs:
+            retrieved_context = "\n\n".join([doc.page_content for doc in context_docs])
+            system_prompt = """
+            You're an expert assitant. This application is desired to respond to
+            the user queries on Australian Privacy laws, regulations, frameworks,
+            policies and best practices. Please the context to answer the question.
+            """
 
-    def query(self, user_query: str) -> str:
+            messages = [
+                SystemMessage(content=system_prompt),
+                HumanMessage(content=f"{retrieved_context}\n\nQuestion: {query}")
+            ]
+            llm = ChatOllama(model=MODEL_NAME, 
+                             temperature=TEMPERATURE,
+                             num_ctx=2048)
+            response = llm.invoke(messages)
+            return response.content
+        
+        else:
+            logger.warning("No context documents retrieved; generating response without context.")
+            return None
+
+    def user_interaction(self, user_query: str = "What is privacy?") -> str:
         """
         End-to-end RAG query:
         1. Retrieve relevant documents
@@ -106,7 +129,15 @@ class RAGPipeline:
         Returns:
             str: Generated answer.
         """
-        logger.info(f"Processing query through RAGPipeline: {user_query}")
-        retrieved_docs = self.retrieve(user_query)
-        answer = self.generate_answer(user_query, retrieved_docs)
-        return answer
+        new_query = str(input("\n\nPlease ask your question: "))
+        if new_query: 
+            user_query = new_query
+        else:
+            logger.info("No input provided. Using default query.")
+        context_docs = self.filter_retrieved_context(query=user_query)
+        response = self.generate_response(query=user_query, context_docs=context_docs)
+        if response:
+            return response
+        else:
+            logger.info(f"\nNo relevant response is found. Please try again")
+            return None
