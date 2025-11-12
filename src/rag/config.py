e73721daf5fdf9f6d5fef25c04b495d8c61ca09d
Abdul Bari <abdulbari80@gmail.com>
2025-11-12 17:25:51 +1100

    new variables added

--- src/rag/config.py
+++ src/rag/config.py
@@ -1,13 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    data_dir: str = "data"
-    chunk_size : int = 200
-    chunk_overlap: int = 50
-    faiss_index_dir: str = "vector_store/faiss_index"
+    data_dir: str = "data/privacy"
+    chunk_size : int = 500
+    chunk_overlap: int = 100
+    faiss_index_dir: str = "vector_store_au/faiss_index"
     embedding_model_name: str = "all-MiniLM-L6-v2"
-    llm_model_name: str = "gemma:2b"
+    llm_model_name: str = "llama3.2:3b"
     top_k: int = 3
+    llm_temperature: float = 0.2
+    threshold: float = 0.5
 
 settings = Settings()
 
